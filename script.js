4a8d5690b24e8007fe0a94b3f7a46ad6dac78f5c
jal0115 <B242270016@must.edu.mn>
2026-09-17 18:07:04 -0400

    script.js-ийг цэвэрлэж шинэчлэв, .gitignore нэмэв

--- script.js
+++ script.js
@@ -1,16 +1,7 @@
 import http from 'k6/http';
 import { sleep, check } from 'k6';
 
-export const options = {
-  vus: 5,
-  duration: '30s',
-  thresholds: {
-    // Жишээ нь: 95% хүсэлтийн хугацаа 564ms-ээс бага байх ёстой (PASS болох босго)
-    http_req_duration: ['p(95) < 10'],
-    // Алдааны хувь 1%-аас бага байх ёстой
-    http_req_failed: ['rate < 0.01'],
-  },
-};
+export const options = { vus: 5, duration: "30s" };
 
 export default function () {
   const res = http.get('https://test.k6.io');
